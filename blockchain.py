da802a961136c5afddf9b53ea80ce074d1c4a286
iamisu <isurika.ak@gmail.com>
2022-01-28 23:29:09 +0530

    default arguments in a function

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,12 @@
-blockchain = [[1]]
+blockchain = []
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), 5])
+def add_value(transaction_amount,last_transaction):
+    blockchain.append([last_transaction,transaction_amount])
     print(blockchain)
 
-add_value(2)
-add_value(10)
-add_value(0.2)
-
+add_value(2,[1])
+add_value(10, get_last_blockchain_value())
+add_value(0.2,get_last_blockchain_value())
