aadb8cfd64246a1d040570e6382e8740ead8f853
iamisu <isurika.ak@gmail.com>
2022-01-28 21:37:06 +0530

    adding arguments to functions

--- blockchain.py
+++ blockchain.py
@@ -3,10 +3,11 @@ blockchain = [[1]]
 def get_last_blockchain_value():
     return blockchain[-1]
 
-def add_value():
+def add_value(transaction_amount):
     blockchain.append([get_last_blockchain_value(), 5])
     print(blockchain)
 
-add_value()
-add_value()
-add_value()
+add_value(2)
+add_value(10)
+add_value(0.2)
+
