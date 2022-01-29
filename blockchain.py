d32d57860a96cf854b9bbfdd1d0cb6ad302ae46d
iamisu <isurika.ak@gmail.com>
2022-01-29 14:13:16 +0530

    comments & doc strings

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,21 @@
 blockchain = []
 
 def get_last_blockchain_value():
+    """Returns the last value of the current block chain"""
     return blockchain[-1]
 
 def add_value(transaction_amount,last_transaction=[-1]):
+    """Append a new value as well as the last blockchain value to the blockchain
+    
+    Arguments:
+        :transaction_amount :the amount that should be added
+
+        :last_transaction : the last blockchain transaction.(default[1])
+        """
     blockchain.append([last_transaction,transaction_amount])
 
 def get_user_input():
+    """Returns the input of the user.(a new transaction amount)"""
     return float(input('plz enter your transaction amount'))
     
 tx_amount = get_user_input()
