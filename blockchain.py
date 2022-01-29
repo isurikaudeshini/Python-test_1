c49fdb23dc1929aad224da2634e1487109b4ad8c
iamisu <isurika.ak@gmail.com>
2022-01-29 12:36:40 +0530

    using the  input function

--- blockchain.py
+++ blockchain.py
@@ -3,10 +3,14 @@ blockchain = []
 def get_last_blockchain_value():
     return blockchain[-1]
 
-def add_value(transaction_amount,last_transaction):
+def add_value(transaction_amount,last_transaction=[-1]):
     blockchain.append([last_transaction,transaction_amount])
-    print(blockchain)
+    
+tx_amount = float(input('plz enter your transaction amount'))
+add_value(tx_amount)
+tx_amount = float(input('plz enter your transaction amount'))
+add_value(tx_amount, get_last_blockchain_value())
+tx_amount = float(input('plz enter your transaction amount'))
+add_value(tx_amount,get_last_blockchain_value())
 
-add_value(2,[1])
-add_value(10, get_last_blockchain_value())
-add_value(0.2,get_last_blockchain_value())
+print(blockchain)
