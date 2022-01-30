620a06670164736e444152d946a91544f1bf35d4
iamisu <isurika.ak@gmail.com>
2022-01-30 22:36:32 +0530

    Manipulate the chain

--- blockchain.py
+++ blockchain.py
@@ -1,27 +1,28 @@
+#initializing the list
 blockchain = []
-
 
 def get_last_blockchain_value():
     """Returns the last value of the current block chain"""
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
-
-def add_value(transaction_amount, last_transaction=[-1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """Append a new value as well as the last blockchain value to the blockchain
 
     Arguments:
         :transaction_amount :the amount that should be added
         :last_transaction : the last blockchain transaction.(default[1])
-        """
+    """
+    if last_transaction == None:
+            last_transaction =[1] 
     blockchain.append([last_transaction, transaction_amount])
-
 
 def get_transaction_value():
     """Returns the input of the user.(a new transaction amount)"""
     user_input = float(input('plz enter your transaction amount:  '))
 
     return user_input
-
 
 def get_user_choice():
     user_input = input('Your choice:  ')
@@ -33,24 +34,23 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting block')
         print(block)
+   
     
-
-
-#Get the first transaction input and add the value to the blockchain
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
 while True:
     print('Please choose')
     print('1: Add a new transaction value ')
     print('2: Output the blockchain blocks ')
+    print('h: Manipulate the chain ')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
+        add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
         break
     else:
