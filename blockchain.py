c3e331256afd10f35040f4879f8ffc1da2c108d1
iamisu <isurika.ak@gmail.com>
2022-01-30 12:53:18 +0530

    if-else

--- blockchain.py
+++ blockchain.py
@@ -17,29 +17,43 @@ def add_value(transaction_amount, last_transaction=[-1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     """Returns the input of the user.(a new transaction amount)"""
-    return float(input('plz enter your transaction amount'))
+    user_input = float(input('plz enter your transaction amount:  '))
+
+    return user_input
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
-
-""" tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(),
-          transaction_amount=tx_amount)   #Command pallete-> format doc (structured with indentation)
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())"""
+def get_user_choice():
+    user_input = input('Your choice:')
+    return user_input
 
 
-while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
-
+def print_blockchain_elements():
     # output the blockchain list to the console
     for block in blockchain:
         print('Outputting block')
         print(block)
+    
+
+
+#Get the first transaction input and add the value to the blockchain
+tx_amount = get_transaction_value()
+add_value(tx_amount)
+
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value ')
+    print('2: Output the blockchain blocks ')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value)
+    else:
+        print_blockchain_elements()
+
+
+  
 
 
 print('Done!')
