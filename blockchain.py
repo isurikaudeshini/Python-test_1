50160031dcee1e3c05bfda9aa4b973353bcdde07
iamisu <isurika.ak@gmail.com>
2022-01-31 15:24:05 +0530

    hashing previous blocks

--- blockchain.py
+++ blockchain.py
@@ -4,7 +4,7 @@ genesis_block ={
     'index': 0,
     'transactions': []
 }
-blockchain = []
+blockchain = [genesis_block]
 open_transactions = []
 owner = 'Isurika'
 
@@ -34,8 +34,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 
 def mine_block():
     last_block = blockchain[-1]
+    hashed_block = ''
+    for key in last_block:
+        value = last_block[key]
+        hashed_block = hashed_block + str(value)
+
     block = {
-        'previous hash': 'xyz',
+        'previous hash': hashed_block,
         'index': len(blockchain),
         'transactions': open_transactions
     }
@@ -96,7 +101,8 @@ waiting_for_input = True
 while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value ')
-    print('2: Output the blockchain blocks ')
+    print('2: Mine a new block ')
+    print('3: Output the blockchain blocks ')
     print('h: Manipulate the chain ')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -106,6 +112,8 @@ while waiting_for_input:
         add_transaction(recipient, amount=amount)
         print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
@@ -114,10 +122,10 @@ while waiting_for_input:
         waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list. ')
-    if not verify_chain():
-        print_blockchain_elements()
-        print('Invalid blockchain, please try again.')
-        break
+    # if not verify_chain():
+    #     print_blockchain_elements()
+    #     print('Invalid blockchain, please try again.')
+    #     break
 else:
     print('User left')
 
