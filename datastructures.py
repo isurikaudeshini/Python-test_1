4916a18eb26597c8be110a18fe18190bc8a0c792
iamisu <isurika.ak@gmail.com>
2022-02-06 22:09:34 +0530

    Dictionayr, tuple, set -itarbel methods

--- datastructures.py
+++ datastructures.py
@@ -1,3 +1,14 @@
 simple_list =[1,2,3,4]
 simple_list.extend([5,6,7])
 print (simple_list)
+
+d = {'name': 'Isurika'}  # dictionary
+print(d.items())
+for k, v in d.items():
+    print(k, v)
+
+    t = (1,2,3)   # tuple
+    print(t.index(1))
+
+    s = {'Isurika', 'Anna', 'Isurika'} # set
+    print(s)
