0bf0ce9e7218c0aa3805e0db53517135a6f1cf1e
iamisu <isurika.ak@gmail.com>
2022-02-07 18:10:28 +0530

    Unpacking dictionary with **

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
-def unlimited_arguments(*args):   #star syntax is used to convert function arguments to a tuple of arguments #Unpacking
-    print(args)
-    for argument in args:
-        print(argument)
+def unlimited_arguments(*args, **keyword_args):   #star syntax is used to convert function arguments to a tuple of arguments #Unpacking
+    print(keyword_args)
+    for k, argument in keyword_args.items():
+        print(k, argument)
 
-unlimited_arguments(1,2,3,4) # a tuple
-unlimited_arguments([1,2,3,4])
-unlimited_arguments(*[1,2,3,4])
+unlimited_arguments(1,2,3,4, name = 'Isurika', age =29) # a tuple
+#unlimited_arguments([1,2,3,4])
+#unlimited_arguments(*[1,2,3,4])
