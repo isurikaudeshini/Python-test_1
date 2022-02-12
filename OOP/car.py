a733151690dd380bc3c76a050017e8ed01cfe9bc
iamisu <isurika.ak@gmail.com>
2022-02-12 17:11:42 +0530

    Understanding class attributes

--- OOP/car.py
+++ OOP/car.py
@@ -1,8 +1,20 @@
 class Car:
-    top_speed = 100.0
+    top_speed = 100  #attribute
+    warnings = []
 
     def drive(self): #self is not a reserved keywoord , convention is to go with self. i.e. access variables within the class
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
 
 car1 = Car()  #Constructor, Construct a new instance of Car class
 car1.drive() #method on that object
+
+car1.warnings.append('New warning')
+#Car.top_speed = 200
+
+car2 = Car()
+car2.drive() #method on that object
+print(car2.warnings)
+
+car3 = Car()
+car3.drive() #method on that object
+print(car3.warnings)
