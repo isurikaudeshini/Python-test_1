fd98023eaec56492e3660b30dd5bab1b17152085
iamisu <isurika.ak@gmail.com>
2022-02-12 18:20:05 +0530

    Classes with special methods

--- OOP/car.py
+++ OOP/car.py
@@ -2,8 +2,12 @@ class Car:
     # top_speed = 100  #attribute
     # warnings = []
     def __init__(self,starting_top_speed=100):
-        self.top_speed = starting_top_speed
+        self.top_speed = starting_top_speed    #Instance attribute
         self.warnings = []
+
+    def __repr__(self):
+        print('Printing..')
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed,len(self.warnings))
 
     def drive(self): #self is not a reserved keywoord , convention is to go with self. i.e. access variables within the class
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
@@ -12,6 +16,8 @@ car1 = Car()  #Constructor, Construct a new instance of Car class
 car1.drive() #method on that object
 
 car1.warnings.append('New warning')
+#print(car1.__dict__)
+print(car1)
 #Car.top_speed = 200
 
 car2 = Car(200)
