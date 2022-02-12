dac9579fb139e30772e62f56b97ec5d070d2c964
iamisu <isurika.ak@gmail.com>
2022-02-12 17:48:25 +0530

    Constructor and Instance attributes

--- OOP/car.py
+++ OOP/car.py
@@ -1,6 +1,9 @@
 class Car:
-    top_speed = 100  #attribute
-    warnings = []
+    # top_speed = 100  #attribute
+    # warnings = []
+    def __init__(self,starting_top_speed=100):
+        self.top_speed = starting_top_speed
+        self.warnings = []
 
     def drive(self): #self is not a reserved keywoord , convention is to go with self. i.e. access variables within the class
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
@@ -11,10 +14,10 @@ car1.drive() #method on that object
 car1.warnings.append('New warning')
 #Car.top_speed = 200
 
-car2 = Car()
+car2 = Car(200)
 car2.drive() #method on that object
 print(car2.warnings)
 
-car3 = Car()
+car3 = Car(250)
 car3.drive() #method on that object
 print(car3.warnings)
