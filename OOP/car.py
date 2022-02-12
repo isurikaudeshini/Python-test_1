f8770a20f8cfcad5ba096ee40738ab2ed85421cf
iamisu <isurika.ak@gmail.com>
2022-02-12 19:56:17 +0530

    Private & public attributes

--- OOP/car.py
+++ OOP/car.py
@@ -3,11 +3,18 @@ class Car:
     # warnings = []
     def __init__(self,starting_top_speed=100):
         self.top_speed = starting_top_speed    #Instance attribute
-        self.warnings = []
+        self.__warnings = []
 
     def __repr__(self):
         print('Printing..')
-        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed,len(self.warnings))
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed,len(self.__warnings))
+
+    def add_warning(self, warning_text):
+        if len(warning_text) > 0:
+            self.__warnings.append(warning_text) #double underscore in front is used to tell private variable or private attribute
+                                                 #can access only form inside the class
+    def get_warnings(self):
+        return self.__warnings
 
     def drive(self): #self is not a reserved keywoord , convention is to go with self. i.e. access variables within the class
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
@@ -15,15 +22,16 @@ class Car:
 car1 = Car()  #Constructor, Construct a new instance of Car class
 car1.drive() #method on that object
 
-car1.warnings.append('New warning')
+car1.add_warning('New warning')
+# car1.__warnings.append('[]')
 #print(car1.__dict__)
 print(car1)
 #Car.top_speed = 200
 
 car2 = Car(200)
 car2.drive() #method on that object
-print(car2.warnings)
+print(car2.get_warnings)
 
 car3 = Car(250)
 car3.drive() #method on that object
-print(car3.warnings)
+print(car3.get_warnings)
