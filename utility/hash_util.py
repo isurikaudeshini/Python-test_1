30329547d0c951156c82db18d4f4b6df4f226ca5
iamisu <isurika.ak@gmail.com>
2022-02-16 09:10:38 +0530

    _ using underscore to not to be exported element from a package

--- utility/hash_util.py
+++ utility/hash_util.py
@@ -1,4 +1,4 @@
-import hashlib as hl
+import hashlib as _hl
 import json  
 
 def hash_string_256(string):
@@ -7,4 +7,4 @@ def hash_string_256(string):
 def hash_block(block):
     hashable_block = block.__dict__.copy()
     hashable_block['transactions'] = [tx.to_ordered_dict() for tx in hashable_block['transactions']]
-    return hl.sha256(json.dumps(hashable_block, sort_keys=True).encode()).hexdigest()
+    return _hl.sha256(json.dumps(hashable_block, sort_keys=True).encode()).hexdigest()
