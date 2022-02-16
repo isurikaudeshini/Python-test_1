90406273355ae4255e13b8e6ed524544fea4eb61
iamisu <isurika.ak@gmail.com>
2022-02-16 22:09:32 +0530

    Connecting the node & the wallet

--- utility/hash_util.py
+++ utility/hash_util.py
@@ -9,4 +9,4 @@ def hash_string_256(string):
 def hash_block(block):
     hashable_block = block.__dict__.copy()
     hashable_block['transactions'] = [tx.to_ordered_dict() for tx in hashable_block['transactions']]
-    return _hl.sha256(json.dumps(hashable_block, sort_keys=True).encode()).hexdigest()
+    return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
--- wallet.py
+++ wallet.py
@@ -4,9 +4,17 @@ import binascii
 
 class Wallet:
     def __init__(self):
-        private_key, public_key = self.genereate_keys
+        
+        self.private_key = None
+        self.public_key = None
+
+    def create_keys(self):
+        private_key, public_key = self.genereate_keys()
         self.private_key = private_key
         self.public_key = public_key
+    
+    def load_keys(self):
+        pass
 
     def genereate_keys(self):
         private_key = RSA.generate(1024, Crypto.Random.new(), read)
