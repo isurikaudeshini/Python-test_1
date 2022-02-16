d583f8f5b313ee986a45d01a2cb4da1df82015a9
iamisu <isurika.ak@gmail.com>
2022-02-17 00:43:14 +0530

    Saving & loading keys to files

--- node.py
+++ node.py
@@ -8,7 +8,8 @@ class Node:
     def __init__(self):
         # self.wallet.public_key = str(uuid4())
         self.wallet = Wallet()
-        self.blockchain = None
+        self.wallet.create_keys()
+        self.blockchain = Blockchain(self.wallet.public_key) 
        
     def get_transaction_value(self):
         """Returns the input of the user.(a new transaction amount)"""
@@ -47,6 +48,7 @@ class Node:
             print('4: Check transaction validity ')
             print('5: Create wallet ')
             print('6: Load wallet ')
+            print('7: Save keys ')
             print('q: Quit')
             user_choice = self.get_user_choice()
             if user_choice == '1':
@@ -71,7 +73,10 @@ class Node:
                 self.wallet.create_keys()
                 self.blockchain = Blockchain(self.wallet.public_key) 
             elif user_choice == '6':
-                pass
+                self.wallet.load_keys()
+                self.blockchain = Blockchain(self.wallet.public_key) 
+            elif user_choice == '7':
+                self.wallet.save_keys()
             elif user_choice == 'q':
                 waiting_for_input = False
             else:
