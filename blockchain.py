c60e36414898bb90273c537d2e99a43e3dfef2c5
iamisu <isurika.ak@gmail.com>
2022-02-18 23:23:36 +0530

    Returning funds

--- blockchain.py
+++ blockchain.py
@@ -87,6 +87,8 @@ class Blockchain:
         return proof
 
     def get_balance(self):
+        if self.hosting_node == None:
+            return None
         participant = self.hosting_node
         tx_sender = [[tx.amount for tx in block.transactions
                     if tx.sender == participant] for block in self.__chain]
