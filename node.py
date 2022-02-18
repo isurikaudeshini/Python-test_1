b5b0820f6e87cb8c73ac5b160d0201431b641680
iamisu <isurika.ak@gmail.com>
2022-02-19 00:46:23 +0530

    Adding a User Interface

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
 from wallet import Wallet
@@ -8,6 +8,10 @@ app = Flask(__name__)
 wallet = Wallet()
 blockchain = Blockchain(wallet.public_key)
 CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_ui():
+    return send_from_directory('ui', 'node.html')
 
 @app.route('/wallet', methods=['POST'])
 def create_keys():
@@ -60,9 +64,6 @@ def get_balance():
         }
         return jsonify(response), 500
 
-@app.route('/', methods=['GET'])
-def get_ui():
-    return 'this works!'
 
 @app.route('/transaction', methods=['POST'])
 def add_transaction():
